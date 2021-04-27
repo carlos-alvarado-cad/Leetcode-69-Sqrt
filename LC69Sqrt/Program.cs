aa27fee4cb020c07fc057f6b77db0dc05bcfa0df
Carlos Alvarado <wikiwakawakawee@gmail.com>
2021-04-26 22:41:12 -0500

    Add project files.

---
+++ LC69Sqrt/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LC69Sqrt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetSqrt(8));
+        }
+
+        public static int GetSqrt(int num)
+        {
+            if (num <= 0)
+            {
+                return 0;
+            }
+            return (int)Math.Sqrt(num);
+        }
+    }
+}
